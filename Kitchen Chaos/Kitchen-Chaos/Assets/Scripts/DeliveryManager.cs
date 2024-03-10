da8e75e4b48751486082ae3ac50ff07b5c01df92
Quang <quanghm@kis-v.com.vn>
2024-03-10 09:42:40 +0700

    init

---
+++ Kitchen Chaos/Kitchen-Chaos/Assets/Scripts/DeliveryManager.cs
@@ -0,0 +1,103 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeliveryManager : MonoBehaviour
+{
+    public event EventHandler OnRecipeSpawned;
+    public event EventHandler OnRecipeCompleted;
+    public event EventHandler OnRecipeSuccess;
+    public event EventHandler OnRecipeFailed;
+
+    public static DeliveryManager Instance{get; private set;} 
+
+    [SerializeField] private RecipeListSO recipeListSO;
+    private List<RecipeSO> waitingRecipeSOList;
+    private float spawnRecipeTimer;
+    private float spawnRecipeTimerMax = 4f;
+    private int waitingRecipesMax = 4;
+    private int successfulRecipesAmount;
+
+    [Header("Recipe Runtime")]
+    [SerializeField] private List<KitchenObjectSO> kitchenObjectSOList;
+    private FireBaseManager _fireBaseManager;
+
+    private void Awake()
+    {
+        Instance = this;
+        _fireBaseManager = GetComponent<FireBaseManager>();
+        waitingRecipeSOList = new List<RecipeSO>();    
+    }
+
+    public void OrderRecipe(string recipeName)
+    {
+        RecipeSO orderRecipe = null;
+        foreach (var recipe in recipeListSO.recipeSOList)
+        {
+            if(recipe.recipeName == recipeName)
+            {
+                orderRecipe = recipe; 
+                break;
+            }
+        }
+
+        if (KitchenGameManager.Instance.IsGamePlaying() && waitingRecipeSOList.Count < waitingRecipesMax)
+        {
+            RecipeSO waitingRecipeSO = orderRecipe;
+            waitingRecipeSOList.Add(waitingRecipeSO);
+
+            OnRecipeSpawned?.Invoke(this, EventArgs.Empty);
+        }
+    }
+
+    public void DeliverRecipe(PlateKitchenObject plateKitchenObject)
+    {
+        for(int i = 0; i < waitingRecipeSOList.Count; i++)
+        {
+            RecipeSO waitingRecipeSO = waitingRecipeSOList[i];
+
+            if(waitingRecipeSO.kitchenObjectSOList.Count == plateKitchenObject.GetKitchenObjectSOList().Count)
+            {
+                bool plateContentsMatchesRecipe = true;
+                foreach(KitchenObjectSO recipeKitchenObjectSO in waitingRecipeSO.kitchenObjectSOList)
+                {
+                    bool ingredientFound = false;
+                    foreach(KitchenObjectSO plateKitchenObjectSO in plateKitchenObject.GetKitchenObjectSOList())
+                    {
+                        if(plateKitchenObjectSO == recipeKitchenObjectSO)
+                        {
+                            ingredientFound = true;
+                            break;
+                        }
+                    }
+                    if(!ingredientFound)
+                    {
+                        plateContentsMatchesRecipe = false;
+                    }
+                }
+                if(plateContentsMatchesRecipe)
+                {
+                    successfulRecipesAmount++;
+
+                    waitingRecipeSOList.RemoveAt(i);
+
+                    OnRecipeCompleted? .Invoke(this, EventArgs.Empty);
+                    OnRecipeSuccess? .Invoke(this, EventArgs.Empty);
+
+                    return;
+                }
+            }
+        }
+        OnRecipeFailed? .Invoke(this, EventArgs.Empty);
+    }
+
+    public List<RecipeSO> GetWaitingRecipeSOList()
+    {
+        return waitingRecipeSOList;
+    }
+
+    public int GetSuccessfulRecipesAmount()
+    {
+        return successfulRecipesAmount;
+    }
+}
---
+++ Kitchen Chaos/Kitchen-Chaos/Assets/Scripts/FireBaseManager.cs
@@ -0,0 +1,113 @@
+﻿using Firebase;
+using Firebase.Database;
+using Firebase.Extensions;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class FireBaseManager : MonoBehaviour
+{
+    DatabaseReference reference;
+    private List<FirebaseRecipeData> activeRecipe;
+
+    void Start()
+    {
+        FirebaseApp.CheckAndFixDependenciesAsync().ContinueWithOnMainThread(task =>
+        {
+            if (task.Exception != null)
+            {
+                Debug.LogError($"Firebase initialization failed: {task.Exception}");
+                return;
+            }
+            reference = FirebaseDatabase.DefaultInstance.RootReference;
+
+            RemoveAllRecipeData();
+            FetchRecipeData();
+            ListenForRecipeDataChanges();
+        });
+       
+    }
+
+    void ListenForRecipeDataChanges()
+    {
+        reference.Child("recipeData").ChildAdded += Order;
+    }
+
+    private void Order(object sender, ChildChangedEventArgs args)
+    {
+        FetchRecipeData();
+        UpdateRecipe();
+    }
+
+    private void UpdateRecipe()
+    {
+        if (activeRecipe.Count <= 0)
+        {
+            return;
+        }
+        DeliveryManager.Instance.OrderRecipe(activeRecipe[activeRecipe.Count - 1].value);
+    }
+
+    void FetchRecipeData()
+    {
+        activeRecipe = new List<FirebaseRecipeData>();
+        reference.Child("recipeData").GetValueAsync().ContinueWithOnMainThread(task =>
+        {
+            if (task.IsFaulted || task.IsCanceled)
+            {
+                Debug.LogError($"Failed to fetch recipe data: {task.Exception}");
+                return;
+            }
+
+            DataSnapshot snapshot = task.Result;
+
+            if (snapshot != null && snapshot.Exists)
+            {
+                foreach (DataSnapshot childSnapshot in snapshot.Children)
+                {
+                    string key = childSnapshot.Key;
+                    string value = childSnapshot.Value.ToString();
+
+                    FirebaseRecipeData recipeData = new FirebaseRecipeData(key, value);
+                    activeRecipe.Add(recipeData);
+                    Debug.Log($"Recipe key: {key}, value: {value}");
+                }
+                Debug.Log("????: " + activeRecipe.Count);
+                UpdateRecipe();
+            }
+            else
+            {
+                Debug.LogWarning("No recipe data found.");
+            }
+        });
+    }
+
+    void RemoveAllRecipeData()
+    {
+        reference.Child("recipeData").RemoveValueAsync().ContinueWithOnMainThread(task =>
+        {
+            if (task.IsFaulted || task.IsCanceled)
+            {
+                Debug.LogError($"Failed to remove all recipe data: {task.Exception}");
+                return;
+            }
+
+            Debug.Log("All recipe data removed successfully.");
+        });
+    }
+}
+
+[System.Serializable]
+public class FirebaseRecipeData
+{
+    public string key;
+    public string value;
+
+    public FirebaseRecipeData(string key, string value)
+    {
+        this.key = key;
+        this.value = value;
+    }
+}
